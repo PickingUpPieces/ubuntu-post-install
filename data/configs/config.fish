024b827bb16a12f20affb1814a77ff0ff7870b8d
hackatop <v.picking@gmx.de>
2019-07-13 21:58:18 +0200

    Added Terminals

--- data/configs/config.fish
+++ data/configs/config.fish
@@ -1,5 +1,7 @@
 set -Ux EDITOR nvim
 
+set -g theme_color_scheme solarized-dark
+
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
